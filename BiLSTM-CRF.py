eaffd2b525e814c3483d84e5c33869c3481f58fe
nantanic@stanford.edu <nantanick@Nantanicks-MacBook-Pro.local>
2019-03-01 20:26:28 -0800

    changed lookup

--- BiLSTM-CRF.py
+++ BiLSTM-CRF.py
@@ -150,13 +150,13 @@ def log_sum_exp(vec):
 
 
 class BiLSTM_CRF(nn.Module):
-    def __init__(self, vocab_size, tag_to_ix, embedding_dim, hidden_dim):
+    def __init__(self, vocab_size, tag2ix, embedding_dim, hidden_dim):
         super(BiLSTM_CRF, self).__init__()
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
         self.vocab_size = vocab_size
-        self.tag_to_ix = tag_to_ix
-        self.tagset_size = len(tag_to_ix)
+        self.tag2ix = tag2ix
+        self.tagset_size = len(tag2ix)
 
         self.word_embeds = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2,
@@ -172,8 +172,8 @@ class BiLSTM_CRF(nn.Module):
 
         # These two statements enforce the constraint that we never transfer
         # to the start tag and we never transfer from the stop tag
-        self.transitions.data[tag_to_ix[START_TAG], :] = -10000
-        self.transitions.data[:, tag_to_ix[STOP_TAG]] = -10000
+        self.transitions.data[tag2ix[START_TAG], :] = -10000
+        self.transitions.data[:, tag2ix[STOP_TAG]] = -10000
 
         self.hidden = self.init_hidden()
 
@@ -185,7 +185,7 @@ class BiLSTM_CRF(nn.Module):
         # Do the forward algorithm to compute the partition function
         init_alphas = torch.full((1, self.tagset_size), -10000.)
         # START_TAG has all of the score.
-        init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+        init_alphas[0][self.tag2ix[START_TAG]] = 0.
 
         # Wrap in a variable so that we will get automatic backprop
         forward_var = init_alphas
@@ -208,7 +208,7 @@ class BiLSTM_CRF(nn.Module):
                 # scores.
                 alphas_t.append(log_sum_exp(next_tag_var).view(1))
             forward_var = torch.cat(alphas_t).view(1, -1)
-        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        terminal_var = forward_var + self.transitions[self.tag2ix[STOP_TAG]]
         alpha = log_sum_exp(terminal_var)
         return alpha
 
@@ -223,11 +223,11 @@ class BiLSTM_CRF(nn.Module):
     def _score_sentence(self, feats, tags):
         # Gives the score of a provided tag sequence
         score = torch.zeros(1)
-        tags = torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+        tags = torch.cat([torch.tensor([self.tag2ix[START_TAG]], dtype=torch.long), tags])
         for i, feat in enumerate(feats):
             score = score + \
                 self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
-        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+        score = score + self.transitions[self.tag2ix[STOP_TAG], tags[-1]]
         return score
 
     def _viterbi_decode(self, feats):
@@ -235,7 +235,7 @@ class BiLSTM_CRF(nn.Module):
 
         # Initialize the viterbi variables in log space
         init_vvars = torch.full((1, self.tagset_size), -10000.)
-        init_vvars[0][self.tag_to_ix[START_TAG]] = 0
+        init_vvars[0][self.tag2ix[START_TAG]] = 0
 
         # forward_var at step i holds the viterbi variables for step i-1
         forward_var = init_vvars
@@ -259,7 +259,7 @@ class BiLSTM_CRF(nn.Module):
             backpointers.append(bptrs_t)
 
         # Transition to STOP_TAG
-        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        terminal_var = forward_var + self.transitions[self.tag2ix[STOP_TAG]]
         best_tag_id = argmax(terminal_var)
         path_score = terminal_var[0][best_tag_id]
 
@@ -270,7 +270,7 @@ class BiLSTM_CRF(nn.Module):
             best_path.append(best_tag_id)
         # Pop off the start tag (we dont want to return that to the caller)
         start = best_path.pop()
-        assert start == self.tag_to_ix[START_TAG]  # Sanity check
+        assert start == self.tag2ix[START_TAG]  # Sanity check
         best_path.reverse()
         return path_score, best_path
 
@@ -307,21 +307,26 @@ HIDDEN_DIM = 4
 # )]
 training_data = [(list("ประเพณีการเทศน์มหาชาติ"), list("0000001001000010000001"))]
 
-char_to_ix = {}
-for sentence, tags in training_data:
-    for char in sentence:
-        if char not in char_to_ix:
-            char_to_ix[char] = len(char_to_ix)
+thai_chars = "กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮฯะั าำิ ี ึ ื ุ ู ฺ฿เแโใไๅๆ็ ่ ้ ๊ ๋ ์ ํ ๎๐๑๒๓๔๕๖๗๘๙"
+eng_chars = " !\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}"
+extra_chars = "αβ"
+all_chars = thai_chars+eng_chars+extra_chars
+char2ix = {c:i for i,c in enumerate(list(all_chars))}
+ix2char = {i:c for i,c in enumerate(list(all_chars))}
+# for sentence, tags in training_data:
+#     for char in sentence:
+#         if char not in char2ix:
+#             char2ix[char] = len(char2ix)
 
-tag_to_ix = {"0": 0, "1": 1, START_TAG: 2, STOP_TAG: 3}
+tag2ix = {"0": 0, "1": 1, START_TAG: 2, STOP_TAG: 3}
 
-model = BiLSTM_CRF(len(char_to_ix), tag_to_ix, EMBEDDING_DIM, HIDDEN_DIM)
+model = BiLSTM_CRF(len(char2ix), tag2ix, EMBEDDING_DIM, HIDDEN_DIM)
 optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay=1e-4)
 
 # Check predictions before training
 with torch.no_grad():
-    precheck_sent = prepare_sequence(training_data[0][0], char_to_ix)
-    precheck_tags = torch.tensor([tag_to_ix[t] for t in training_data[0][1]], dtype=torch.long)
+    precheck_sent = prepare_sequence(training_data[0][0], char2ix)
+    precheck_tags = torch.tensor([tag2ix[t] for t in training_data[0][1]], dtype=torch.long)
     print(model(precheck_sent))
 
 # Make sure prepare_sequence from earlier in the LSTM section is loaded
@@ -334,8 +339,8 @@ for epoch in range(
 
         # Step 2. Get our inputs ready for the network, that is,
         # turn them into Tensors of word indices.
-        sentence_in = prepare_sequence(sentence, char_to_ix)
-        targets = torch.tensor([tag_to_ix[t] for t in tags], dtype=torch.long)
+        sentence_in = prepare_sequence(sentence, char2ix)
+        targets = torch.tensor([tag2ix[t] for t in tags], dtype=torch.long)
 
         # Step 3. Run our forward pass.
         loss = model.neg_log_likelihood(sentence_in, targets)
@@ -347,7 +352,7 @@ for epoch in range(
 
 # Check predictions after training
 with torch.no_grad():
-    precheck_sent = prepare_sequence(training_data[0][0], char_to_ix)
+    precheck_sent = prepare_sequence(training_data[0][0], char2ix)
     print(model(precheck_sent))
 # We got it!
 
