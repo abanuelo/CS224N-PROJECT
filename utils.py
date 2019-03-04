c82f2527e210d775bc659441446ed7fc50d02ec2
nantanic@stanford.edu <nantanick@Nantanicks-MacBook-Pro.local>
2019-03-03 20:39:12 -0800

    added batching function

---
+++ utils.py
@@ -0,0 +1,61 @@
+import torch
+from itertools import zip_longest
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.optim as optim
+
+def batch_iter(data, batch_size:int, shuffle=False):
+    """ Yield batches of source and target sentences reverse sorted by length (largest to smallest).
+    @param data (list of (src_sent, tgt_sent)): list of tuples containing source and target sentence
+    @param batch_size (int): batch size
+    @param shuffle (boolean): whether to randomly shuffle the dataset
+
+    modified from assignment 4, CS224N starter code
+    """
+    batch_num = math.ceil(len(data) / batch_size)
+    index_array = list(range(len(data)))
+
+    if shuffle:
+        np.random.shuffle(index_array)
+
+    for i in range(batch_num):
+        indices = index_array[i * batch_size: (i + 1) * batch_size]
+        batch = [data[idx] for idx in indices]
+        batch = sorted(batch, key=lambda e: len(e[0]), reverse=True)
+        yield batch
+
+
+def pad_ids(ids:List[List[int]], pad_id):
+	"""
+	Pads an id list to the longest sentence
+
+	@param ids a list of lists of ids 
+	@param pad_id a list of ids that is padded 
+	"""
+	max_len = len(ids[0])
+	sents_padded = []
+
+    for sent in sents:
+        sents_padded.append(sent + [pad_id]*(max_len-len(sent)))
+
+    return sents_padded 
+
+def sents2tensor(sents: List[str], char2ix:dict, pad_id:int, device: torch.device):
+	"""
+	Creates a padded tensor from a list of sentences
+
+	@param sents -- a list of strings
+	@param char2ix -- a dictionary from character to id
+	@param pad_id -- id of padtoken
+	@param device -- device od the model
+
+	@return a tensor of dim (max_string_length, batch_size)
+	"""
+	ids = [[char2ix[c] for c in s] for s in sents]
+	padded = pad_ids(ids)
+	return torch.tensor(padded, dtype=torch.long, device=device)
+
+
+
+
+
