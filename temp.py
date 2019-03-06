837b07615132ef92221a755a3126afe055f641dd
nantanic@stanford.edu <nantanick@Nantanicks-MacBook-Pro.local>
2019-03-05 23:16:35 -0800

    completed forward (debug needed)

--- temp.py
+++ temp.py
@@ -2,7 +2,8 @@
 
 
 Combined code from:
-
+https://pytorch.org/tutorials/beginner/nlp/advanced_tutorial.html
+https://github.com/threelittlemonkeys/lstm-crf-pytorch
 """
 
 
@@ -41,9 +42,9 @@ class BiLSTM(nn.Module):
 		lengths = mask.sum(1).int()
 		X_packed = pack_padded_sequence(X, lengths, batch_first = True)
 		hidden, _ = self.lstm(X_packed)
-		hidden, _ = pad_packed_sequence(hidden,padding_value = self.pad_id, batch_first = True)
-		h_out = self.hidden2tag(hidden)
-		#h_out *= mask.unsqueeze(2)
+		hidden, _ = pad_packed_sequence(hidden, padding_value = self.pad_id, batch_first = True)
+		h_out = self.hidden2tag(hidden) #(batch_size, max_sent_len, num_tag)
+		h_out *= mask.unsqueeze(2)
 		return h_out
 		
 
@@ -67,28 +68,48 @@ class CRF(nn.Module):
 		self.trans.data[:, stop_id] = -10000. # no transition from end except to padding
 		self.trans.data[:, pad_id] = -10000. # no transition from pad except to pading
 		self.trans.data[pad_id, :] = -10000. # no transition to padding except from end
-		self.trans.data[pad_id, stop_id] = 0.
-		self.trans.data[pad_id, pad_id] = 0.
+		self.trans.data[pad_id, stop_id] = 0. # stop to pad
+		self.trans.data[pad_id, pad_id] = 0. #pad to pad
 
 	def forward(self, h_tag, mask): #(batch_size, max_sent_len, tag_size)????
 		#initialize alphas 
+		bptr = torch.tensor([],dtype=torch.long)
 		score = torch.full((self.batch_size, self.num_tags), -10000., dtype=torch.float)
 		score[:, self.stop_id] = 0. #set the stop score to 0
-		trans = self.trans.unsqueeze(0) #(1,num_tags,num_tags)
+		#trans = self.trans.unsqueeze(0) #(1,num_tags,num_tags)
 
 		# iterate over sentence (max_sent_len)
 		for t in range(h_tag.size(1)):  
 			mask_t = mask[:, t].unsqueeze(1) #get t'th mask (batch_size, 1, 1)
 			emit_t = h_tag[:, t].unsqueeze(2) # (batch_size, num_tags, 1)
-			score_t = score.unsqueeze(1) + emit_t + trans 
-						# (batch_size, 1, num_tags) -> (batch_size, num_tags, num_tags)
-			score_t = log_sum_exp(score_t) # [B, C, C] -> [B, C]
+			score_t_trans = score.unsqueeze(1) + self.trans
+			score_x, bptr_t = score_t_trans.max(2)
+			bptr = torch.cat((bptr, bptr_t.unsqueeze(1)), 1)
+			score_t = score_t_trans + emit_t
+			score_t = log_sum_exp(score_t) # [batch_size, num_tags, num_tags] -> [batch_size, num_tags]
 			score = score_t * mask_t + score * (1 - mask_t)
-		score = log_sum_exp(score + self.trans[self.stop_id])
-		return score # partition function
+		score += self.trans[self.stop_id]
+		best_score, best_tag = score.max(1)
+		score = log_sum_exp(score)
 
-	def decode(self):
-		pass
+		# print(bptr)
+		# print(bptr.size())
+		# print(best_tag)
+		bptr = bptr.tolist()
+		best_path = [[i] for i in best_tag.tolist()]
+		print(best_path)
+		for b in range(self.batch_size):
+			x = best_tag[b] # best tag
+			y = int(mask[b].sum().item()) #get length
+			for bptr_t in reversed(bptr[b][:y]):
+				x = bptr_t[x]
+				best_path[b].append(x)
+			best_path[b].pop() #pop the start token
+			best_path[b].reverse()
+		return score, best_path
+
+	# def decode(self):
+	# 	pass
 
 
 
@@ -102,9 +123,10 @@ class BiLSTM_CRF(nn.Module):
 	def forward(self, input):
 		mask = 1-sents.data.eq(pad_id).float()
 		h_out = self.lstm(input, mask)
+		return crf(h_out, mask)
 
-	def decode(self):
-		pass
+	# def decode(self):
+	# 	pass
 
 
 	@staticmethod
