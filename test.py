a110971e8b864e7fcdf36c4fd2635993ae3d9756
nantanic@stanford.edu <nantanick@Nantanicks-MacBook-Pro.local>
2019-03-09 22:47:51 -0800

    prep for pull

--- test.py
+++ test.py
@@ -26,24 +26,26 @@ new_model = BiLSTM_CRF(len(char2ix), len(tag2ix), embed_size, hidden_size, tag2i
 
 old_model = old_BiLSTM_CRF(char2ix, tag2ix_old, embed_size, hidden_size, tag2ix_old['σ'], tag2ix_old['ε'])
 
-for sents, gold in batch_iter(data, batch_size):
-    sents_tensor=sents2tensor(sents, char2ix, char2ix['π'], device)
-    gold_tensor=sents2tensor(gold, tag2ix, tag2ix['π'], device)
-    mask = 1-sents_tensor.data.eq(char2ix['π']).float()
-    print(sents_tensor.size(), mask.size(), gold_tensor.size())
-    score = new_model(sents_tensor, gold_tensor, mask)
-    loss = torch.mean(score)
-    loss.backward()
-    print('score: ',score, 'loss:', loss)
-    score.backwards()
-    print('######')
-    for i in range(len(sents)):
-        gold_old = torch.tensor([tag2ix_old[c] for c in gold[i]])
-        sents_old = torch.tensor([char2ix[c] for c in sents[i]])
-        loss = old_model.neg_log_likelihood(sents_old, gold_old)
+for e in range(100):
+    for sents, gold in batch_iter(data, batch_size):
+        sents_tensor=sents2tensor(sents, char2ix, char2ix['π'], device)
+        gold_tensor=sents2tensor(gold, tag2ix, tag2ix['π'], device)
+        mask = 1-sents_tensor.data.eq(char2ix['π']).float()
+        score = new_model(sents_tensor, gold_tensor, mask)
+        loss = torch.mean(score)
         loss.backward()
-        print(loss)
-    print('---------------------------------------------------')
+        if (e%5==0):
+            print('score: ',score, 'loss:', loss)
+            print('######')
+        for i in range(len(sents)):
+            gold_old = torch.tensor([tag2ix_old[c] for c in gold[i]])
+            sents_old = torch.tensor([char2ix[c] for c in sents[i]])
+            loss = old_model.neg_log_likelihood(sents_old, gold_old)
+            loss.backward()
+            if (e%5==0):
+                print(loss)
+        if (e%5==0):
+            print('---------------------------------------------------')
 
 
 
