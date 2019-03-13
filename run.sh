b1a083edd74a2b15e8b92f3a4880f19522c3930c
abanuelo <32311654+abanuelo@users.noreply.github.com>
2019-03-13 01:04:27 -0700

    checked if model overfits on data

--- run.sh
+++ run.sh
@@ -7,9 +7,9 @@ elif [ "$1" = "test" ]; then
     touch outputs/test_outputs.txt
     CUDA_VISIBLE_DEVICES=0 python run.py test --save-to=./models/model.bin --test-input=./data/test.txt --test-gold=./data/test_output_gold.txt --cuda
 elif [ "$1" = "train_local" ]; then
-	touch model.bin
+	touch ./models/model.bin
 	python run.py train --train-input=./data/train_small.txt --train-gold=./data/train_small_output_gold.txt \
-        --batch-size=5 --valid-niter=100 --max-epoch=60 --save-to=./models/model.bin --dev-input=./data/dev_small.txt --dev-gold=./data/dev_small_output_gold.txt
+        --batch-size=3 --valid-niter=100 --max-epoch=120 --save-to=./models/model.bin --dev-input=./data/dev_small.txt --dev-gold=./data/dev_small_output_gold.txt
 elif [ "$1" = "test_local" ]; then
 	mkdir -p outputs
 	#rm -f outputs/test_local_outputs.txt
